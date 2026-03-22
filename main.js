04c34876de0cb5b8bd080997dd3106f6f4f13c77
qaz5358985 <qaz5358985@gmail.com>
2026-03-22 12:05:46 +0000

    initial commit

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-// Add JS here
+const generateBtn = document.getElementById("generate-btn");
+const lottoNumbersContainer = document.querySelector(".lotto-numbers");
+
+generateBtn.addEventListener("click", () => {
+  lottoNumbersContainer.innerHTML = "";
+  const numbers = new Set();
+  while (numbers.size < 6) {
+    numbers.add(Math.floor(Math.random() * 45) + 1);
+  }
+
+  numbers.forEach((number) => {
+    const lottoNumber = document.createElement("div");
+    lottoNumber.classList.add("lotto-number");
+    lottoNumber.textContent = number;
+    lottoNumbersContainer.appendChild(lottoNumber);
+  });
+});
